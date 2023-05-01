26d5d8c5bf9c62b58aca779650fc9d8a922586e7
Helen Wang <wangyixuan@msn.com>
2023-05-01 11:46:03 -0700

    first commit

---
+++ flask_app/controllers/cookies_controller.py
@@ -0,0 +1,56 @@
+from flask import render_template,request,redirect
+from flask_app import app
+
+from flask_app.models.cookie import Cookie
+
+
+@app.route("/")
+def index():
+    # call the get all classmethod to get all friends
+    cookies = Cookie.get_all()
+    print(cookies)
+    return render_template("index.html", all_cookies = cookies)
+
+@app.route('/cookie/new')
+def display_new():
+    return render_template("new_order.html")
+
+@app.route('/create_cookie', methods=["POST"])
+def create_cookie():
+    #add validation after reading validation part
+    if not Cookie.is_valid_cookie(request.form):
+        print("Validation Fail")
+        return redirect('/cookie/new')
+    print(request.form)
+    id = Cookie.save(request.form)
+    return redirect("/")
+
+@app.route('/show_edit/<int:cookie_id>')
+def show_edit(cookie_id):
+    one_cookie = Cookie.get_one(cookie_id)
+    print(one_cookie)
+    return render_template("edit_cookie.html", one_cookie=one_cookie)
+
+@app.route('/update_cookie', methods=["POST"])
+def edit_cookie():
+    cookie_id = request.form['id']
+    if not Cookie.is_valid_cookie(request.form):
+        print("Validation Fail")
+        return redirect(f'show_edit/{cookie_id}')
+    print(request.form)
+    # there is a hidden attribute being transferred through the html form
+    Cookie.update(request.form)
+    return redirect("/")
+
+
+# @app.route('/user/<int:user_id>')
+# def get_oneUser(user_id):
+    
+#     one_user = User.get_one(user_id)
+#     print(one_user)
+#     return render_template("showOne.html", one_user=one_user)
+
+# @app.route('/user/delete/<int:user_id>')
+# def destroy(user_id):
+#     User.delete(user_id)
+#     return redirect('/')
---
+++ flask_app/models/cookie.py
@@ -0,0 +1,94 @@
+# import the function that will return an instance of a connection
+from flask_app.configs.mysqlconnection import connectToMySQL
+# model the class after the friend table from our database
+from flask import flash
+# import re
+# EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+
+class Cookie:
+    DB = 'cookie_flask_schema'
+    def __init__( self , data ):
+        self.id = data['id']
+        self.name = data['name']
+        self.type = data['type']
+        self.num_of_boxes = data['num_of_boxes']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+# Now we use class methods to query our database
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM cookies;"
+# make sure to call the connectToMySQL function with the schema you are targeting.
+        results = connectToMySQL(cls.DB).query_db(query)
+    # Create an empty list to append our instances of friends
+        cookies = []
+    # Iterate over the db results and create instances of friends with cls.
+        for cookie in results:
+            cookies.append( cls(cookie) )
+        return cookies
+    
+    @classmethod
+    def save(cls, data ):
+        query = "INSERT INTO cookies ( name,type, num_of_boxes  ) VALUES ( %(name)s , %(type)s , %(num_of_boxes)s );"
+        # data is a dictionary that will be passed into the save method from server.py
+        # return a id for the newly created record
+        new_id= connectToMySQL(cls.DB).query_db( query, data )
+        return new_id
+    # the get_one method will be used when we need to retrieve just one specific row of the table
+    @classmethod
+    def get_one(cls, cookie_id):
+        query  = "SELECT * FROM cookies WHERE id = %(id)s;"
+        data = {'id':cookie_id}
+        results = connectToMySQL(cls.DB).query_db(query, data)
+        return cls(results[0])
+    
+
+    @classmethod
+    def update(cls,data):
+        query = """UPDATE cookies 
+                SET name=%(name)s,
+                type=%(type)s,num_of_boxes=%(num_of_boxes)s 
+                WHERE id = %(id)s;"""
+        return connectToMySQL(cls.DB).query_db(query,data)
+    
+    @classmethod
+    def delete(cls, cookie_id):
+        query  = "DELETE FROM cookies WHERE id = %(id)s;"
+        data = {"id": cookie_id}
+        return connectToMySQL(cls.DB).query_db(query, data)
+            
+    @staticmethod
+    def is_valid_cookie(cookie):
+        is_valid = True
+        print(f'string length of the input cookies is {len(cookie["num_of_boxes"])}')
+
+        if len(cookie["name"]) <= 0:
+            is_valid = False
+            flash("client name is required.")
+        if len(cookie["type"]) <= 0:
+            is_valid = False
+            flash("Type is required.")
+        if len(cookie["num_of_boxes"]) <= 0:
+            is_valid = False
+            flash("number of boxes is required.")
+
+        if len(cookie["name"]) > 0 and len(cookie["name"]) < 2: 
+            flash("name must be at 2 characters")
+            is_valid = False
+        if len(cookie["type"]) > 0 and len(cookie["type"]) < 2: 
+            flash("type must be at 2 characters")
+            is_valid = False
+        #this way, it won't report the base 10 error when do not input anything in num_of_boxes
+        # if cookie["num_of_boxes"]:#if there is some input
+        #     if int(cookie["num_of_boxes"]) <=0:#then decide if it is less than zero or equals to zero
+        #         is_valid = False
+        #         flash("Please enter a valid number")
+        # else:#do not input any string or number
+        #     flash("Please do not leave num of boxes blank")
+        #     is_valid = False
+
+        print(type(cookie['num_of_boxes']))
+
+        return is_valid
+            
